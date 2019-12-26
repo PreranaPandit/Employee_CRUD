ef350faad184c994c92728bba352c204fd004a87
PreranaPandit <preranapandit07@gmail.com>
2019-12-26 13:00:08 +0545

    design completion with home buttons

--- app/src/main/java/com/example/employee_crud/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/employee_crud/ui/home/HomeFragment.java
@@ -17,12 +17,18 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.employee_crud.R;
 import com.example.employee_crud.ui.dashboard.DashboardFragment;
+import com.example.employee_crud.ui.notifications.NotificationsFragment;
+import com.example.employee_crud.ui.search.SearchFragment;
+import com.example.employee_crud.ui.updatedelete.UpdateDeleteFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
     private Button btnSeeAll;
+    private Button btnAdd;
+    private Button btnSearch;
+    private Button btnUpdateDelete;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -39,6 +45,10 @@ public class HomeFragment extends Fragment {
         });
 
         btnSeeAll = root.findViewById(R.id.btnShowAll);
+        btnAdd = root.findViewById(R.id.btnRegister);
+        btnSearch = root.findViewById(R.id.btnSearch);
+        btnUpdateDelete = root.findViewById(R.id.btnUpdateDelete);
+
 
         btnSeeAll.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +63,41 @@ public class HomeFragment extends Fragment {
                 Fragment someFragment = new DashboardFragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.nav_host_fragment, someFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+            }
+        });
+
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment searchFragment = new SearchFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, searchFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment addFragment = new NotificationsFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, addFragment ); // give your fragment container id in first parameter
+                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                transaction.commit();
+            }
+        });
+
+
+        btnUpdateDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment updateFragment = new UpdateDeleteFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, updateFragment ); // give your fragment container id in first parameter
                 transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
                 transaction.commit();
             }
---
+++ app/src/main/java/com/example/employee_crud/ui/updatedelete/UpdateDeleteViewModel.java
@@ -0,0 +1,7 @@
+package com.example.employee_crud.ui.updatedelete;
+
+import androidx.lifecycle.ViewModel;
+
+public class UpdateDeleteViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
